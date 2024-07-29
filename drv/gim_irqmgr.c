527b4d48beef704724efa3bf2399450779e58941
Tom Day <tom.d@ceracare.co.uk>
2024-07-29 01:37:19 +0100

    syntax

--- drv/gim_irqmgr.c
+++ drv/gim_irqmgr.c
@@ -147,7 +147,7 @@ static int alloc_iv_ring(struct adapter *adapt)
 						PCI_DMA_BIDIRECTIONAL);
 #else
 	ih->ivr_ma.quad_part = dma_map_page(
-						(struct device *)adapt->pf.pci_dev->dev,
+						&adapt->pf.pci_dev->dev,
 						(struct page *)list[0], 0,
 						PAGE_SIZE,
 						DMA_BIDIRECTIONAL);
@@ -166,7 +166,7 @@ static int alloc_iv_ring(struct adapter *adapt)
 							PCI_DMA_BIDIRECTIONAL);
 #else
 	ih->ivr_wptr_wa.quad_part = dma_map_page(
-							(struct device *)adapt->pf.pci_dev->dev,
+							&adapt->pf.pci_dev->dev,
 							(struct page *)list[1], 0,
 							PAGE_SIZE,
 							DMA_BIDIRECTIONAL);
